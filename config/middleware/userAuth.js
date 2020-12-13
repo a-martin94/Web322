c9f664b0fd767aa3a321fb2fbd21f29ae9062035
andrew <andrewmartin380@gmail.com>
2020-12-12 21:17:53 -0500

    Empty

--- config/middleware/userAuth.js
+++
@@ -1,9 +0,0 @@
-const userAccess = (req,res,next) => {
-    if (req.session.userInfo == null) {
-        res.redirect("/user/login");
-    } else {
-        next();
-    }
-}
-
-module.exports = userAccess;
